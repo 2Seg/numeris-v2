7bf988dbc462728b3fa37f3c02bb0982312f08a3
MinhNamNguyen <nguyen.minhnam@hotmail.fr>
2020-07-02 17:26:21 +0200

    [Back] Update president information

---
+++ back/tests/Feature/Message/Index/IndexDeveloperTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature\Message\Index;
+
+use App\Models\Role;
+use Illuminate\Http\JsonResponse;
+use Tests\TestCaseWithAuth;
+
+class IndexDeveloperTest extends TestCaseWithAuth
+{
+    protected $username = Role::DEVELOPER;
+
+    /**
+     * @group message
+     */
+    public function testDeveloperMessageIndex()
+    {
+        $this->json('GET', route('messages.index'))
+            ->assertStatus(JsonResponse::HTTP_OK)
+            ->assertJsonStructure([[
+                'title',
+                'content',
+                'link',
+                'is_active',
+            ]]);
+    }
+}
