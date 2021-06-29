f7be26241ffa55d1bf1e96559505196e993ac82c
unknown <nguyen.minhnam@hotmail.fr>
2021-06-29 21:48:35 +0200

    [FRONT] Equipe 2020/2021
    	modified:   back/Dockerfile
    	modified:   front/src/app/core/constants/team_members.ts
    	modified:   front/src/app/modules/showcase/home/home.component.html
    
    	modified:   back/Dockerfile
    	modified:   back/composer.lock
    	modified:   front/src/app/core/constants/team_members.ts
    	modified:   front/src/app/modules/showcase/home/home.component.html
    
    	modified:   back/Dockerfile
    	modified:   front/src/app/core/constants/team_members.ts
    	modified:   front/src/app/modules/showcase/home/home.component.html
    
    	modified:   back/Dockerfile
    	modified:   front/src/app/core/constants/team_members.ts
    	modified:   front/src/app/modules/showcase/home/home.component.html

--- front/src/app/core/constants/team_members.ts
+++ front/src/app/core/constants/team_members.ts
@@ -2,25 +2,25 @@ import { TeamMember } from '../classes/team-member';
 
 export const TEAM_MEMBERS: TeamMember[] = [
   new TeamMember(
-    'Candice',
-    'Rumeau',
-    'Présidente',
-    'candylol17',
-    'team/2019-2020/candice-rumeau.jpg',
+    'Maxime',
+    'Schuchmann',
+    'Président',
+    'maxime.schuch',
+    'team/2019-2020/maxime-schuchmann.jpg',
   ),
   new TeamMember(
-    'Toan',
-    'Dang Ngoc',
+    'Valentin',
+    'Ghuilhem',
     'Vice-président',
-    'toan.dangngoc.94',
-    'team/2019-2020/toan-dang-ngoc.jpg',
+    'valentin.guilhem',
+    'team/2019-2020/valentin-guilhem.jpg',
   ),
   new TeamMember(
-    'Tchong-kite',
-    'Huam',
+    'Alexandre',
+    'Ulku',
     'Secrétaire général',
-    'tchong.kite.h',
-    'team/2019-2020/tchong-kite-huam.jpg',
+    'alexandre.ulku.7',
+    'team/2019-2020/alexandre-ulku.jpg',
   ),
   new TeamMember(
     'Elise',
@@ -30,32 +30,11 @@ export const TEAM_MEMBERS: TeamMember[] = [
     'team/2019-2020/elise-savornin.jpg',
   ),
   new TeamMember(
-    'Jeanne',
-    'Fricaudet',
-    'Vice-trésorière',
-    'jeanne.fricaudet',
-    'team/2019-2020/jeanne-fricaudet.jpg'
-  ),
-  new TeamMember(
     'David',
     'Ni',
-    'Vice-trésorier',
+    'Trésorier',
     'David.Ni.1108',
     'team/2019-2020/david-ni.jpg'
-  ),
-  new TeamMember(
-    'Pierre-Jean',
-    'Drieu La-Rochelle',
-    'Graphiste',
-    'pierrejean.drieu',
-    'team/2019-2020/pierre-jean-drieu-la-rochelle.jpg'
-  ),
-  new TeamMember(
-    'Hashley',
-    'Ramanankatsoina',
-    'Responsable communication',
-    'hleyjr',
-    'team/2019-2020/hashley-ramanankatsoina.jpg'
   ),
   new TeamMember(
     'Oussama',
@@ -65,57 +44,64 @@ export const TEAM_MEMBERS: TeamMember[] = [
     'team/2019-2020/oussama-belhia.jpg'
   ),
   new TeamMember(
-    'Alexandre',
-    'Ulku',
-    'Responsable démarchage',
-    'alexandre.ulku.7',
-    'team/2019-2020/alexandre-ulku.jpg'
-  ),
-  new TeamMember(
-    'Valentin',
-    'Guilhem',
-    'Chargé de contrat',
-    'valentin.guilhem',
-    'team/2019-2020/valentin-guilhem.jpg'
-  ),
-  new TeamMember(
-    'Clémence',
-    'Ouyang',
-    'Chargé de contrat',
-    'clemence.ouyang',
-    'team/2019-2020/clemence-ouyang.jpg'
-  ),
-  new TeamMember(
     'Ali',
     'Qassou',
     'Chargé de contrat',
     'numerisisep',
   ),
   new TeamMember(
-    'Maxime',
-    'Schuchmann',
-    'Chargé de contrat',
-    'maxime.schuch',
-    'team/2019-2020/maxime-schuchmann.jpg'
+    'Clara',
+    'Augié',
+    'Vice-trésorière',
+    'clara.augie.8',
   ),
   new TeamMember(
-    'Théophile',
-    'Valenduc',
-    'Chargé de contrat',
-    '100009793662548',
-    'team/2019-2020/theophile-valenduc.jpg'
+    'Karol',
+    'Kubik',
+    'Graphiste',
+    'karol.kubik.923',
   ),
   new TeamMember(
-    'Eliott',
-    'de Séguier',
-    'DSI',
-    'eliott.deseguier',
-    'team/2019-2020/eliott-deseguier.jpg',
+    'Léna',
+    'Cheam',
+    'Responsable communication',
+    'LenaCheam2',
+  ),
+  new TeamMember(
+    'Gaspard',
+    'Cotterot',
+    'Chargé de contrat',
+    'gaspard.cotterot',
+  ),
+  new TeamMember(
+    'Thomas',
+    'Preget',
+    'Chargé de contrat',
+    'thomas.preget',
+  ),
+  new TeamMember(
+    'Valentine',
+    'Le Saint',
+    'Chargé de contrat',
+    'valentine.lesaint.7',
+  ),
+
+  new TeamMember(
+    'Alexandre',
+    'Nge',
+    'Chargé de contrat',
+    'alexandre.nge',
   ),
   new TeamMember(
     'Minh Nam',
     'Nguyen',
     'Assistant DSI',
     'minhnam.nguyen',
+  ),
+  new TeamMember(
+    'Yona',
+    'Bitton',
+    'Assistante DSI',
+    'yona.cullen',
   ),
 ];
